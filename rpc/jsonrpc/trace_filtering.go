8ddf90b7a35ae6fd39ce7973bde8d40ebc812cb8
Angel Valkov <avalkov@polygon.technology>
2025-10-07 19:09:24 +0300

    Add paritytrace result

--- rpc/jsonrpc/trace_filtering.go
+++ rpc/jsonrpc/trace_filtering.go
@@ -214,6 +214,7 @@ func (api *TraceAPIImpl) Block(ctx context.Context, blockNr rpc.BlockNumber, gas
 			pt.BlockNumber = &blockNum
 			pt.TransactionHash = trace.TransactionHash
 			pt.TransactionPosition = &txpos
+			pt.Result = trace.Output // THIS
 			out = append(out, *pt)
 		}
 	}

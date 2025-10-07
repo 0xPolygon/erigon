28805b1991709aaaeaab08ae179122834fee7ffe
Angel Valkov <avalkov@polygon.technology>
2025-10-07 17:50:07 +0300

    Remove gasBailOut

--- rpc/jsonrpc/trace_filtering.go
+++ rpc/jsonrpc/trace_filtering.go
@@ -173,7 +173,6 @@ func (api *TraceAPIImpl) Block(ctx context.Context, blockNr rpc.BlockNumber, gas
 	if gasBailOut == nil {
 		gasBailOut = new(bool) // false by default
 	}
-	*gasBailOut = true
 	tx, err := api.kv.BeginTemporalRo(ctx)
 	if err != nil {
 		return nil, err

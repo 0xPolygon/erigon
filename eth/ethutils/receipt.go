ef85fac05f57ecf99f1fadf77f493c8d15752cf4
marcello33 <marcelloardizzone@hotmail.it>
2025-10-08 15:27:55 +0200

    enforce zero effectiveGasPrice on ssTx

--- eth/ethutils/receipt.go
+++ eth/ethutils/receipt.go
@@ -76,6 +76,9 @@ func MarshalReceipt(
 		gasPrice := new(big.Int).Add(header.BaseFee, txn.GetEffectiveGasTip(baseFee).ToBig())
 		fields["effectiveGasPrice"] = (*hexutil.Big)(gasPrice)
 	}
+	if chainConfig.Bor != nil && txn.Type() == types.StateSyncTxType {
+		fields["effectiveGasPrice"] = (*hexutil.Big)(big.NewInt(0))
+	}
 
 	// Assign receipt status.
 	fields["status"] = hexutil.Uint64(receipt.Status)

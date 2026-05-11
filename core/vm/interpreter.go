e49fac84cf14becdbb67efc35ee956153a933fcb
marcello33 <marcelloardizzone@hotmail.it>
2026-05-11 18:22:01 +0200

    core, execution, p2p, polygon: chicago hf for v3.6.0 release

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -181,6 +181,8 @@ type EVMInterpreter struct {
 func NewEVMInterpreter(evm *EVM, cfg Config) *EVMInterpreter {
 	var jt *JumpTable
 	switch {
+	case evm.chainRules.IsChicago:
+		jt = &chicagoInstructionSet
 	case evm.chainRules.IsLisovoPro:
 		jt = &lisovoProInstructionSet
 	case evm.chainRules.IsLisovo:
--- polygon/bor/borcfg/bor_config.go
+++ polygon/bor/borcfg/bor_config.go
@@ -52,6 +52,7 @@ type BorConfig struct {
 	LisovoBlock       *big.Int `json:"lisovoBlock"`       // Lisovo switch block (nil = no fork, 0 = already on Lisovo)
 	LisovoProBlock    *big.Int `json:"lisovoProBlock"`    // LisovoPro switch block (nil = no fork, 0 = already on LisovoPro)
 	GiuglianoBlock    *big.Int `json:"giuglianoBlock"`    // Giugliano switch block (nil = no fork, 0 = already on Giugliano)
+	ChicagoBlock      *big.Int `json:"chicagoBlock"`      // Chicago switch block (nil = no fork, 0 = already on Chicago)
 
 	StateSyncConfirmationDelay map[string]uint64         `json:"stateSyncConfirmationDelay"` // StateSync Confirmation Delay, in seconds, to calculate `to`
 	Coinbase                   map[string]common.Address `json:"coinbase"`                   // coinbase address
@@ -242,6 +243,14 @@ func (c *BorConfig) IsGiugliano(number uint64) bool {
 
 func (c *BorConfig) GetGiuglianoBlock() *big.Int {
 	return c.GiuglianoBlock
+}
+
+func (c *BorConfig) IsChicago(number uint64) bool {
+	return isForked(c.ChicagoBlock, number)
+}
+
+func (c *BorConfig) GetChicagoBlock() *big.Int {
+	return c.ChicagoBlock
 }
 
 func (c *BorConfig) CalculateStateSyncDelay(number uint64) uint64 {
